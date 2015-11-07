037fb5256f75ab756793925903b030c462cf5ce4
wiggidywouter <wiggidywouter@yandex.com>
2015-11-07 10:48:20 +0100

    Homework-0 part2 completed

--- Homework-0-Ruby-intro/lib/ruby_intro.rb
+++ Homework-0-Ruby-intro/lib/ruby_intro.rb
@@ -21,11 +21,11 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[^aeiou\d]/i === s
 end
 
 def binary_multiple_of_4? s
